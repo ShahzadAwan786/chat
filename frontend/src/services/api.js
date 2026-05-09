af53ecc3be830cb32802336ec9d073c7f4fa4ce9
Shehzad Ashraf <stewardmax5331@gmail.com>
2026-05-09 17:40:23 +0500

    fix: update apiUrl to support fallback for production environment

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,6 +1,8 @@
 import axios from "axios";
 
-const apiUrl = `http://localhost:8000/api`;
+const apiUrl =
+  `http://localhost:8000/api` ||
+  `https://unique-peace-production-5915.up.railway.app/api`;
 
 const getToken = () => localStorage.getItem("auth-token");
 
